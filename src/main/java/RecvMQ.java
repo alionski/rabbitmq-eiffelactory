96d44b27cf79e27aef48319fdaaeb376669c82f5
Alena Hramyka <alenah@axis.com>
2019-04-03 16:06:30 +0200

    Support kill threads; read configs from file
    Change-Id: Iaae3f18d06e5ce0c146a2e2da93f0dc353fcdbe2

--- src/main/java/RecvMQ.java
+++ src/main/java/RecvMQ.java
@@ -3,27 +3,27 @@ import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.DeliverCallback;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.FileWriter;
+import java.io.*;
 import java.sql.Timestamp;
 import java.util.Date;
+import java.util.Properties;
 import java.util.concurrent.TimeoutException;
 
 public class RecvMQ {
 
     private final static String QUEUE_NAME = "eiffelactory";
     private final static String EXCHANGE_NAME = "eiffel";
+    private volatile boolean alive = true;
 
-    public void startReceiving() {
+    public void startReceiving() throws IOException {
+        FileWriter errorFileWriter = new FileWriter ("/tmp/java.log",  true);
+        PrintWriter errorWriter = new PrintWriter(errorFileWriter);
+
         new Thread(new Runnable() {
             @Override
             public void run() {
                 try {
-                    ConnectionFactory factory = new ConnectionFactory();
-                    factory.setHost("localhost");
-                    Connection connection = null;
-                    connection = factory.newConnection();
+                    Connection connection = initConnection();
                     Channel channel = connection.createChannel();
 
                     channel.exchangeDeclare(EXCHANGE_NAME, "fanout");
@@ -38,14 +38,67 @@ public class RecvMQ {
                         PrintWriter writer = new PrintWriter(fWriter);
                         writer.println(res);
                         writer.close();
+                        if (!alive) {
+                            try {
+//                                channel.queueUnbind(queueName, EXCHANGE_NAME, "");
+                                channel.close();
+                                connection.close();
+                            } catch (TimeoutException e) {
+                                e.printStackTrace();
+                                errorWriter.println(e);
+                                errorWriter.close();
+                            }
+                        }
                     };
                     channel.basicConsume(queueName, true, deliverCallback, consumerTag -> { });
-                } catch (IOException e) {
+                } catch (IOException | TimeoutException e) {
                     e.printStackTrace();
-                } catch (TimeoutException e) {
-                    e.printStackTrace();
+                    errorWriter.println(e.getMessage() + e.getCause());
+                    errorWriter.close();
                 }
             }
         }).start();
+    }
+
+    private Connection initConnection() throws IOException, TimeoutException {
+        FileWriter errorFileWriter = new FileWriter ("/tmp/java.log",  true);
+        PrintWriter errorWriter = new PrintWriter(errorFileWriter);
+        Properties prop = new Properties();
+        FileInputStream config = null;
+        String username = null, password = null, vhost = null, hostname = null;
+        int port = 0;
+
+        try {
+            config = new FileInputStream("/etc/secrets.properties");
+            prop.load(config);
+            username = prop.getProperty("username");
+            password = prop.getProperty("password");
+            vhost = prop.getProperty("vhost");
+            hostname = prop.getProperty("hostname");
+            port = Integer.valueOf(prop.getProperty("port"));
+        } catch (IOException ex) {
+            errorWriter.println(ex);
+            errorWriter.close();
+        } finally {
+            if (config != null) {
+                try {
+                    config.close();
+                } catch (IOException e) {
+                    errorWriter.println(e);
+                    errorWriter.close();
+                }
+            }
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.setUsername(username);
+            factory.setPassword(password);
+            factory.setVirtualHost(vhost);
+            factory.setHost(hostname);
+            factory.setPort(port);
+            return factory.newConnection();
+        }
+    }
+
+    public void stopReceiving() {
+        alive = false;
     }
 }
